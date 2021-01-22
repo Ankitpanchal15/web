8b3081a21a467b5c942880bf79be58157cb6bfda
Ankit <panchalankit07@gmail.com>
2021-01-22 13:33:04 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter} from 'react-router-dom';
 
 
 ReactDOM.render(
- <BrowserRouter>
+   <BrowserRouter basename={window.location.pathname || ''}>
     <App />
     </BrowserRouter>
  ,
