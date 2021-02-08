22c0c5b0ade197ee1f4e111d5cc92d52ad8dccf4
Ankit Panchal <76251693+Ankitpanchal15@users.noreply.github.com>
2021-02-08 23:20:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import {BrowserRouter} from 'react-router-dom';
 
 
 ReactDOM.render(
-   <BrowserRouter basename={window.location.pathname || ''}>
     <App />
-    </BrowserRouter>
  ,
   document.getElementById('root')
 );
